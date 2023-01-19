a0352312229d8335a453f79bafa8af051e9ae208
Muskanyadav01 <121680274+Muskanyadav01@users.noreply.github.com>
2023-01-19 22:39:35 +0530

    submitting answer 5 of assignment 1

---
+++ a5_q5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"enter a number for which you have to count the digits."<<endl;
+    cin >> n;
+    //write your code here
+      cout<<"total digits in the number is:"<<n%10<<endl;
+    
+    return 0;
+    
+} 
